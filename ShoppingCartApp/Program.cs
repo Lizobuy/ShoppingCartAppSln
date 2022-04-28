f9744f904746061716508c4502d8d4c0440c561e
Lizobuy <102720671+Lizobuy@users.noreply.github.com>
2022-04-28 10:35:40 +0200

    Add project files.

---
+++ ShoppingCartApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoppingCartApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ShoppingLibrary/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShoppingLibrary
+{
+    public class ShoppingCart
+    {
+        List <Xbox> Xbox = new List <Xbox> ()
+
+        List <PS4> Xbox = new List <PS4> ()
+
+        {
+
+        }
+    }
+}
